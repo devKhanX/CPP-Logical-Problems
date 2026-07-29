6f269090423c54718108fe3d99e0e069ea88ee24
Owais Khan <mowaiskhandev@gmail.com>
2026-07-29 14:59:39 +0500

    Add Longest Winning Streak calculation program
    This program calculates the longest winning streak based on user input for game outcomes.

---
+++ Longest-Winning-Streak.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int nR;
+    int winS = 0;
+    int largestS = 0;
+    cout << "Enter number of rounds: ";
+    cin >> nR;
+    vector<char> arr(nR);
+    cout << "Enter outcomes (W for win, L for loss) separated by spaces:\n";
+    for (int i = 0; i < nR; i++) {
+        cin >> arr[i]; 
+    }
+
+    for (int i = 0; i < nR; i++) {
+        if (arr[i] == 'W' || arr[i] == 'w') {
+            winS++; 
+            
+            if (winS > largestS) {
+                largestS = winS;
+            }
+        } else {
+            winS = 0; 
+        }
+    }
+    cout << "Longest streak: " << largestS << endl;
+    return 0;
+}
