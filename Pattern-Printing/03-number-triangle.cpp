7764c6b0fcc666ee2a901703c956942c83b7b2ea
Owais Khan <mowaiskhandev@gmail.com>
2026-08-24 23:33:07 +0500

    Add number triangle pattern printing program

---
+++ Pattern-Printing/03-number-triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void printPattern (int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<j+1;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter size "<<endl;
+    cin>>n;
+    printPattern(n);
+}
