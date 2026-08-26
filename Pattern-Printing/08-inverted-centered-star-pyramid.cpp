36de6336e5ebd406902348cd3ad2b1ed4af8f564
Owais Khan <mowaiskhandev@gmail.com>
2026-08-26 12:16:28 +0500

    Add inverted centered star pyramid pattern program

---
+++ Pattern-Printing/08-inverted-centered-star-pyramid.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void printPattern(int n) 
+{ 
+    		for (int i=0;i<n;i++) 
+			{
+            for (int j=0;j<i;j++) 
+			{
+                cout << " ";
+            }
+            for (int j = 0; j<2*n-(2 * i + 1);j++) 
+			{
+                cout << "*";
+            }
+            for (int j=0;j<i;j++) 
+			{
+                cout<< " ";
+            }
+            cout << endl;
+        }
+}
+int main() 
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n; 
+    printPattern(n);   
+    return 0; 
+}
